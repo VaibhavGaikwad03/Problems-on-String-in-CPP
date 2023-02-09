9f4323beb3d181904889cd26a64da4e36a12b18e
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-02-09 10:12:16 +0530

    Add files via upload

---
+++ Concat_String.cpp
@@ -0,0 +1,42 @@
+/*
+
+Write a program which accepts 2 strings from user and concat second string after first string. (Implement strcat() function).
+
+Input : "Marvellous Infosystems"
+Input : "Logic Building"
+
+Output : "Marvellous Infosystems Logic Building"
+
+*/
+
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+
+string ConcatString(string &dest, const string &src)
+{
+    return dest += src;
+}
+
+int main(void)
+{
+    string str1, str2;
+    string s;
+
+    cout << "Enter first string : \n";
+    getline(cin, str1);
+
+    cout << "Enter second string : \n";
+    getline(cin, str2);
+
+    s = ConcatString(str1, str2);
+
+    cout << s << endl
+         << str1 << endl;
+
+    return 0;
+}
